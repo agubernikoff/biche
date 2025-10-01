467a76d5de700e3210c74f545a11ebfa03d35047
Sam Gubernikoff <sgubernikoff@gmail.com>
2025-10-01 16:18:09 -0400

    sitemaps

--- app/routes/sitemap-editorial[.]xml.jsx
+++ app/routes/sitemap-editorial[.]xml.jsx
@@ -1,4 +1,4 @@
-import {sanityClient} from '../sanity/sanityClient';
+import {sanityClient} from '~/sanity/sanityClient';
 
 const EDITORIAL_SITEMAP_QUERY = `*[_type == "editorial" && !(_id in path("drafts.**"))]{
   "slug": slug.current,
@@ -6,8 +6,10 @@ const EDITORIAL_SITEMAP_QUERY = `*[_type == "editorial" && !(_id in path("drafts
   publishedAt
 } | order(publishedAt desc)`;
 
-export async function loader() {
+export async function loader({request}) {
   const articles = await sanityClient.fetch(EDITORIAL_SITEMAP_QUERY);
+  const url = new URL(request.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -15,7 +17,7 @@ ${articles
   .map(
     (article) => `
   <url>
-    <loc>https://biche.world/editorial/${article.slug}</loc>
+    <loc>${baseUrl}/editorial/${article.slug}</loc>
     <lastmod>${new Date(article._updatedAt || article.publishedAt).toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
