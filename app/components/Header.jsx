3db41b51bf179abc6c92a7f68549505a938816e9
Alexander Gubernikoff <agubernikoff@gmail.com>
2026-01-10 20:08:17 -0500

    init anim

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -234,7 +234,7 @@ function CartBadge({count, inAside}) {
           gap: '0.25rem',
         }}
       >
-        <AnimatePresence mode="popLayout">
+        <AnimatePresence mode="popLayout" initial={false}>
           {isOpen && !inAside ? (
             <motion.span
               key="close"
