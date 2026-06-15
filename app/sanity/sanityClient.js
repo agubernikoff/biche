1921162d39330ce574a52ead7de95dca66010e1d
Alexander Gubernikoff <agubernikoff@gmail.com>
2026-06-15 18:43:58 -0400

    sanity env reset

--- app/sanity/sanityClient.js
+++ app/sanity/sanityClient.js
@@ -2,7 +2,7 @@ import {createClient} from '@sanity/client';
 
 export const sanityClient = createClient({
   projectId: '3c8olga9',
-  dataset: 'dev',
+  dataset: 'production',
   apiVersion: '2023-10-01',
   useCdn: true,
 });
